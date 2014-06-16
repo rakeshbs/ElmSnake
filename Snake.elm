4f838fe05fe71e0921d8831d90c6c601e1c76821
Rakesh <rakesh@Rakeshs-Mac-Pro.local>
2014-06-16 23:08:26 +0530

    Implemented drawGrid and drawSnake

--- Snake.elm
+++ Snake.elm
@@ -1,10 +1,11 @@
 import Mouse
 
-widthSquare = 30
+widthSquare = 20
 widthCanvas = 320
 heightCanvas = 480
-gridColor = rgba 0 0 0 0.3
-canvasBackgroundColor = rgba 0 0 255 0.4
+gridColor = rgba 0 0 0 1
+canvasBackgroundColor = rgba 0 0 255 0.1
+snakeColor = rgba 100 0 0 0.5
 
 type Vec = (Int,Int)
 
@@ -12,15 +13,27 @@ type SnakeSegment = { position : Vec }
 
 type Snake = {segments : [SnakeSegment]}
 
-main = renderScreen
+defaultSnake = [(4,4),(4,5),(4,6)]
 
-renderVerticalLineAtIndex index =  traced (solid gridColor) <|  segment (index * widthSquare,-1000) (index * widthSquare,1000)
+{----------------------------------------------------------
+Draw the grid
+-----------------------------------------------------------}
 
-renderHorizontalLineAtIndex index =  traced (solid gridColor) <|  segment (-1000,index * widthSquare) (1000,index * widthSquare)
+drawVerticalLineAtIndex index =  traced (solid gridColor) <|  segment (index * widthSquare,-1000) (index * widthSquare,1000)
+
+drawHorizontalLineAtIndex index =  traced (solid gridColor) <|  segment (-1000,index * widthSquare) (1000,index * widthSquare)
 
 
-renderGrid = let verticalLines = map (renderVerticalLineAtIndex) [(-widthCanvas/(2 * widthSquare))..(widthCanvas/(2 * widthSquare))]
-                 horizontalLines =  map (renderHorizontalLineAtIndex) [(-heightCanvas/(2 * widthSquare))..(heightCanvas/(2 * widthSquare))]
-             in verticalLines ++ horizontalLines
+drawGrid = let verticalLines = map (drawVerticalLineAtIndex) [(-widthCanvas/(2 * widthSquare))..(widthCanvas/(2 * widthSquare))]
+               horizontalLines =  map (drawHorizontalLineAtIndex) [(-heightCanvas/(2 * widthSquare))..(heightCanvas/(2 * widthSquare))]
+           in verticalLines ++ horizontalLines
 
-renderScreen = color canvasBackgroundColor <| collage 320 480 renderGrid
+
+drawSnake segments = map (drawSnakeSegment) segments
+
+drawSnakeSegment (x,y) = square  widthSquare |> filled snakeColor |> move (x * widthSquare + (widthSquare/2),y * widthSquare + (widthSquare/2))
+
+drawGame = color canvasBackgroundColor <| collage 320 480 <|  drawGrid  ++ drawSnake defaultSnake
+
+
+main = drawGame
