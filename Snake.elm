fda5fa4de00d07d88d0a9bb05ea6b1c280402f55
Rakesh <rakesh@Rakeshs-MacBook-Pro.local>
2014-06-18 16:10:01 +0530

    Added Keyboard Signal

--- Snake.elm
+++ Snake.elm
@@ -1,11 +1,13 @@
-import Mouse
+import Keyboard
 
-widthSquare = 20
+widthSquare = 10
 widthCanvas = 320
 heightCanvas = 480
 gridColor = rgba 0 0 0 0.7
 canvasBackgroundColor = rgba 0 0 255 0.1
 snakeColor = rgba 100 0 0 0.5
+halfMaxWidth = 8
+halfMaxHeight = 12
 
 data SnakeDirection = Up|Down|Right|Left
 
@@ -17,14 +19,54 @@ type GameState = {snake : Snake}
 
 defaultGame = { snake = defaultSnake }
 
-defaultSnake = { segments = [(4,4),(4,5),(4,6)], direction = Up }
+defaultSnake = { segments = [(4,8),(4,7),(4,6)], direction = Down }
 
-type UserInput = {}
+data Event = GameTick (Float) | KeyboardInput {x : Int, y : Int}
 
-userInput : Signal UserInput
-userInput = constant {}
 
-type Input = { timeDelta:Float, userInput:UserInput }
+boundsX : Int -> Int
+boundsX x =  if | x < -halfMaxWidth -> halfMaxWidth - 1
+                | x >= halfMaxWidth  -> -halfMaxWidth
+                | otherwise         -> x
+
+boundsY : Int -> Int
+boundsY y =  if | y < -halfMaxHeight -> halfMaxHeight - 1
+                | y >= halfMaxHeight  -> -halfMaxHeight
+                | otherwise          -> y
+
+
+moveSnakeHead : Vec -> SnakeDirection -> [Vec]
+moveSnakeHead (x,y) direction = let (newX,newY) =
+                                 case direction of
+                                     Up    -> (x,y-1)
+                                     Down  -> (x,y+1)
+                                     Right -> (x+1,y)
+                                     Left  -> (x-1,y)
+                                in [(boundsX newX,boundsY newY)]
+
+moveSnakeRest : [Vec] -> Vec -> [Vec]
+moveSnakeRest segments prevSeg = let seg  = head segments
+                                     segs = tail segments
+                                 in if | segs == [] -> [prevSeg]
+                                       | otherwise  -> [prevSeg] ++ moveSnakeRest segs seg
+
+moveSnake : Snake -> Snake
+moveSnake snake = let h = head snake.segments
+                  in {snake | segments <- ( moveSnakeHead h snake.direction ++ moveSnakeRest (tail snake.segments) h) }
+
+
+getDirection input = if  | input.x == -1 -> Left
+                         | input.x ==  1 -> Right
+                         | input.y == -1 -> Down
+                         | input.y ==  1 -> Up
+
+changeDirection snake input = {snake | direction <- getDirection input}
+
+stepGame : Event -> GameState -> GameState
+stepGame event g = case event of
+                       GameTick _          -> {g | snake <- moveSnake g.snake }
+                       KeyboardInput input -> {g | snake <- changeDirection g.snake input}
+                       otherwise           -> g
 
 {----------------------------------------------------------
 Draw the grid
@@ -58,12 +100,11 @@ drawGame : GameState -> Element
 drawGame gameState = color canvasBackgroundColor <| collage 320 480
                                 <| drawGrid  ++ drawSnake gameState.snake
 
-delta = fps 30
-input = sampleOn delta (lift2 Input delta userInput)
-interval = every second
+delta = fps 1
+inputdelta = Keyboard.arrows
 
-gameState = foldp stepGame defaultGame input
+gameSignal = merges [lift GameTick delta,lift KeyboardInput inputdelta]
 
-
+gameState = foldp (stepGame) defaultGame <| gameSignal
 
 main = drawGame <~ gameState
