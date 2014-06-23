bffb3c03cebca0b564823816692d3301ddb44e4a
Rakesh <rakesh@Rakeshs-MacBook-Pro.local>
2014-06-23 15:19:54 +0530

    Working on swipe

--- Swipe.elm
+++ Swipe.elm
@@ -9,8 +9,19 @@ data TouchData =  { positionX      : Int,
 
 data TouchState = { touches : [TouchData] }
 
+checkIfTouchEqual touch touchData = let c1 = (touchData.positionX,touchData.positionY,touchData.touchTime)
+                                        c2 = (touch.x0, touch.y0, touch.t0)
+                                    in  c1 == c2
 
+checkIfTouchEnded touches touchData = let touch = head touches
+                                       in if | touch == [] -> False
+                                             | checkIfTouchEqual touch touchData == True -> True
+                                             | otherwise checkIfTouchEnded (tail touches) touchData
 
-analyseTouches =
+analyseTouches = let currentTouches = filter (checkIfTouchEnded touches) touchData
+
 
 main = asText <~  Touch.touches
+
+y = ((1,2,3) == (1,2,3))
+-- > True : Bool
