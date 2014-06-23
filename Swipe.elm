132d3deed981aed2a843e26de782f88d353c9013
Rakesh <rakesh@Rakeshs-Mac-Pro.local>
2014-06-23 22:21:59 +0530

    Still stuck at swipe

--- Swipe.elm
+++ Swipe.elm
@@ -4,24 +4,31 @@ data SwipeDirection = Left|Right|Up|Down|None
 
 data TouchData =  { positionX      : Int,
                     positionY      : Int,
+                    startPositionX : Int,
+                    startPositionY : Int,
                     touchTime      : Int,
                     swipeDirection : SwipeDirection }
 
 data TouchState = { touches : [TouchData] }
 
-checkIfTouchEqual touch touchData = let c1 = (touchData.positionX,touchData.positionY,touchData.touchTime)
+checkIfTouchEqual touch touchData = let c1 = (touchData.startPositionX,touchData.startPositionY,touchData.touchTime)
                                         c2 = (touch.x0, touch.y0, touch.t0)
                                     in  c1 == c2
 
 checkIfTouchEnded touches touchData = let touch = head touches
-                                       in if | touch == [] -> False
-                                             | checkIfTouchEqual touch touchData == True -> True
-                                             | otherwise checkIfTouchEnded (tail touches) touchData
+                                      in if | touch == [] -> False
+                                            | checkIfTouchEqual touch touchData == True -> True
+                                            | otherwise checkIfTouchEnded (tail touches) touchData
 
-analyseTouches = let currentTouches = filter (checkIfTouchEnded touches) touchData
+checkIfNewTouch touch touchState =  let touchData = head touchState
+                                    in if | touchData == [] -> False
+                                          | checkIfTouchEqual touch touchData == True -> True
+                                          | otherwise checkIfNewTouch touch (tail touchState)
 
+
+updateTouches
+
+analyseTouches touches touchState = let currentTouches = filter (checkIfTouchEnded touches) touchState
+                                        newTouches = filter (checkIfNewTouch touches) touchState.touches
 
 main = asText <~  Touch.touches
-
-y = ((1,2,3) == (1,2,3))
--- > True : Bool
